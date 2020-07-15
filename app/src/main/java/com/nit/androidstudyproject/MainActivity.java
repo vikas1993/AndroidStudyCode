5de7457899aca7e9510e92b2c1e106fa814b7829
vikasaggarwal <vk.bkbiet@gmail.com>
2020-07-15 19:57:23 +0530

    Chapter 4

--- app/src/main/java/com/nit/androidstudyproject/MainActivity.java
+++ app/src/main/java/com/nit/androidstudyproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.nit.androidstudyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void shareText(View view) {
+        //Sharing with share sheet
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
+    }
+    public void shareTextCustom(View view) {
+        // creating custom chooser
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+    public void ExplicitIntent(View view) {
+        Intent mExplicitIntent = new Intent(this, SecondActivity.class);
+        startActivity(mExplicitIntent);
+    }
+
+    public void ImplicitIntent(View view) {
+        Intent mImplicitIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:555- 3333"));
+                startActivity(mImplicitIntent);
+
     }
 }
