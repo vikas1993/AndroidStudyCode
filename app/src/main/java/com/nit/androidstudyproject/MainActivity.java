6faafe0dbdc19108f99f90385d6026ad4976a67e
vikasaggarwal <vk.bkbiet@gmail.com>
2020-07-21 19:45:16 +0530

    Chapter 8

--- app/src/main/java/com/nit/androidstudyproject/MainActivity.java
+++ app/src/main/java/com/nit/androidstudyproject/MainActivity.java
@@ -2,20 +2,90 @@ package com.nit.androidstudyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent ;
+//for bound service
+LocalService mService;
+    boolean mBound = false;
+    BroadcastReceiver br;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, HelloService.class);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Bind to LocalService
+        Intent intent = new Intent(this, LocalService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+         br = new MyBroadcastReceiver();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        super.onStop();
+        unbindService(connection);
+        mBound = false;
+        unregisterReceiver(br);
     }
 
     public void startService(View view) {
-        Intent intent = new Intent(this, HelloService.class);
         startService(intent);
+    }
+
+    public void stopService(View view) {
+        stopService(intent);
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    public void boundService(View view) {
+        if (mBound) {
+            // Call a method from the LocalService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            int num = mService.getRandomNumber();
+            Toast.makeText(this, "From bound service number: " + num, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void reciver(View view) {
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(br, filter);
     }
 }
