1e8bebd164cd9cd656a0e60c66a1dae068b4b215
vikasaggarwal <vk.bkbiet@gmail.com>
2020-07-25 00:19:24 +0530

    Chapter 12

--- app/src/main/java/com/nit/androidstudyproject/MainActivity.java
+++ app/src/main/java/com/nit/androidstudyproject/MainActivity.java
@@ -2,13 +2,91 @@ package com.nit.androidstudyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mobileno,message;
+    Button sendsms;
+    private static MainActivity inst;
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mobileno=(EditText)findViewById(R.id.editText1);
+        message=(EditText)findViewById(R.id.editText2);
+        sendsms=(Button)findViewById(R.id.button1);
+
+        sendsms.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String no=mobileno.getText().toString();
+                String msg=message.getText().toString();
+
+                //Getting intent and PendingIntent instance
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+
+                //Get the SmsManager instance and call the sendTextMessage method to send message
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(no, null, msg, pi,null);
+
+                Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        refreshSmsInbox();
+
+    }
+
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        do {
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
+            Log.e("Sms Recieved",str);
+        } while (smsInboxCursor.moveToNext());
+    }
+    public void updateList(final String smsMessage) {
+        Toast.makeText(this, "Message Got +"+smsMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    public void callNo(View view){
+        Intent intent = new Intent (Intent.ACTION_DIAL,Uri.parse("tel:5623451"));
+        startActivity(intent);
+
+    }
+
+    public void sendMail(View view) {
+        startActivity(new Intent(this,SendEmailActivity.class));
     }
 }
